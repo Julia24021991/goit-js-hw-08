0837b1b4e461cdd18963b51ae5d3940c6ca27436
Julia24021991 <yuliya.kovalyk@gmail.com>
2023-11-18 11:31:56 +0200

    commit2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imagesList = document.querySelector('.gallery');
+imagesList.style.listStyle = 'none';
+
+function createImages(image) {
+    return image.map(
+        ({ preview, original, description }) => `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+</li>`
+    ).join('');
+}
+
+imagesList.insertAdjacentHTML('beforeend', createImages(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionDelay: 250,
+    captionsData: 'alt',
+});
+
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const input = form[(name = 'email')];
+const textArea = form[(name = 'message')];
+
+savedStorageValue();
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    if (input.value === '' || textArea.value === '') {
+        return;
+    }
+    const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    console.log(savedText);
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+form.addEventListener('input', throttle(handleInput, 1000));
+
+function handleInput(event) {
+    const userInformation = {
+        email: input.value,
+        message: textArea.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(auserInformation));
+    userInformation[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(userInformation));
+}
+
+function savedStorageValue() {
+    const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedText) {
+        if (savedText.message) {
+            textArea.value = savedText.message;
+        }
+        if (savedText.email) {
+            input.value = savedText.email;
+        }
+    }
+}
